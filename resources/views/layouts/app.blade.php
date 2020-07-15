dfda76b312c7ff536b86b6ccd7a1b5e2d5edf209
MasakiTeshigahara <maskai.teshi@gamail.com>
2020-07-15 16:51:16 +0900

    ボード改善、

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <!-- JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 
     <!-- Fonts -->
@@ -64,9 +62,8 @@
                         </li>
                         @endif
                         @else
-                        <div>
-                        </div>
-                        <a href="{{ url('/home') }}"><img src="/storage/icon/Laravel_SNS.png" alt="ツイート一覧" ,class="center-block" ,width="50" height="50"></a>
+                        <a href="{{ url('/home') }}"><img src="/storage/icon/Laravel_SNS.png" alt="ツイート一覧" ,class="center-block" ,
+                        width="200" height="50" style="margin-right: 220px;"></a>
 
                         <li class="nav-item mr-5">
                             <a href="{{ url('tweets/create') }}" class="btn btn-md btn-primary">ツイートする</a>
@@ -105,6 +102,8 @@
         @yield('content')
     </main>
     </div>
+    <script src="{{ asset('js/Message.js') }}"></script>
+    <script src="moment.js"></script>
 </body>
 
 </html>
